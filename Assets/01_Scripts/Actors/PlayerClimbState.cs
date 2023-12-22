e7558fcaf35b607271e0890b5f32f5fb5defd802
장원익 <somesome2314@gmail.com>
2023-12-22 23:18:02 +0900

    .

---
+++ Assets/01_Scripts/Actors/PlayerClimbState.cs
@@ -0,0 +1,25 @@
+﻿
+public partial class Player
+{
+    // ..?
+    // public const float ClimbMaxStamina = 110;
+    // private const float ClimbUpCost = 100 / 2.2f;
+    // private const float ClimbStillCost = 100 / 10f;
+    // private const float ClimbJumpCost = 110 / 4f;
+    // private const int ClimbCheckDist = 2;
+    // private const int ClimbUpCheckDist = 2;
+    // private const float ClimbNoMoveTime = .1f;
+    // public const float ClimbTiredThreshold = 20f;
+    // private const float ClimbUpSpeed = -45f;
+    // private const float ClimbDownSpeed = 80f;
+    // private const float ClimbSlipSpeed = 30f;
+    // private const float ClimbAccel = 900f;
+    // private const float ClimbGrabYMult = .2f;
+    // private const float ClimbHopY = -120f;
+    // private const float ClimbHopX = 100f;
+    // private const float ClimbHopForceTime = .2f;
+    // private const float ClimbJumpBoostTime = .2f;
+    // private const float ClimbHopNoWindTime = .3f;
+}
+
+
---
+++ Assets/01_Scripts/Actors/PlayerDashState.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+
+public partial class Player
+{
+    // public const int StNormal = 0;
+    // public const int StClimb = 1;
+    // public const int StDash = 2;
+
+    private const float DashSpeed = 240f;
+    private const float EndDashSpeed = 160f; // end speed?
+    private const float EndDashUpMult = .75f;
+    private const float DashTime = .15f;
+    private const float DashCoolDown = .2f;
+    private const float DashRefillCooldown = .1f;
+    private const int DashHJumpThruNudge = 6; // ?
+    private const int DashCornerCorrection = 4;
+    private const int DashVFloorSnapDist = 3; // vertical snap
+    private const float DashAttackTime = .3f; // Attack? => brake hidden wall
+    
+
+    // vars    
+    [HideInInspector] public int Dashes;
+    [HideInInspector] public Vector2? OverrideDashDirection;
+    
+    private float dashCoolDownTimer;
+    private float dashRefillCooldownTimer;
+    [HideInInspector] public Vector2 DashDir;
+
+    private float dashAttackTimer;
+    private bool dashStartedOnGround;
+
+    private bool calledDashEvents;
+    private int lastDashes;
+
+    private float dashTrailTimer;
+
+    public static readonly Color NormalHairColor = new Color(0.67f, 0.2f, 0.2f);
+    public static readonly Color UsedHairColor = new Color(0.27f, 0.72f, 1f);
+    public static readonly Color FlashHairColor = Color.white;
+    
+    
+}
+
+
